0b4d5aaf3ba7a704be2e7ad57b8f35d117faef24
Štěpán Zrník <stepan.zrnik@gmail.com>
2024-03-18 20:23:50 +0100

    new json response

--- src/Content/JsonResponse.php
+++ src/Content/JsonResponse.php
@@ -11,20 +11,42 @@ use Psr\Http\Message\ResponseInterface;
 class JsonResponse
 {
     /**
-     * @param ResponseInterface $response
      * @param object|array<mixed>|scalar|null $responseSchema
-     * @return ResponseInterface
      * @throws JsonException
      * @noinspection PhpPluralMixedCanBeReplacedWithArrayInspection
      */
     public static function of(
         ResponseInterface $response,
         object|array|string|int|float|bool|null $responseSchema,
+        int $statusCode = 200,
     ): ResponseInterface
     {
         $psr17Factory = new Psr17Factory();
         return $response
             ->withHeader('Content-Type', 'application/json')
+            ->withStatus($statusCode)
+            ->withBody(
+                $psr17Factory->createStream(
+                    json_encode($responseSchema, JSON_THROW_ON_ERROR)
+                )
+            );
+    }
+
+    /**
+     * @param object|array<mixed>|scalar|null $responseSchema
+     * @throws JsonException
+     * @noinspection PhpPluralMixedCanBeReplacedWithArrayInspection
+     */
+    public static function new(
+        object|array|string|int|float|bool|null $responseSchema,
+        int $statusCode = 200,
+    ): ResponseInterface
+    {
+        $psr17Factory = new Psr17Factory();
+        return $psr17Factory
+            ->createResponse()
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus($statusCode)
             ->withBody(
                 $psr17Factory->createStream(
                     json_encode($responseSchema, JSON_THROW_ON_ERROR)
